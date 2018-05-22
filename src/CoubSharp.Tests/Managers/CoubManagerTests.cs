0b985239a228c9e26e00cb9f0d0b46473686640d
ansydor <andrii.sydorchuk@gmail.com>
2018-05-22 13:54:32 +0300

    Added CoubSharp.Tests test project Add first test on CoubManagerTests

---
+++ src/CoubSharp.Tests/Managers/CoubManagerTests.cs
@@ -0,0 +1,21 @@
+﻿using CoubSharp.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CoubSharp.Tests.Managers
+{
+    public class CoubManagerTests
+    {
+        [Fact]
+        public async Task CoubManagerThrowsArgumentNullException()
+        {
+            var sut = new CoubManager();
+            await Assert.ThrowsAsync<ArgumentNullException>(()=>sut.GetCoubAsync(null));
+        }
+
+    }
+}
