c387bf2fffe4980a0daeffcf182415c6d799d129
ansydor <andrii.sydorchuk@gmail.com>
2018-06-20 15:32:15 +0300

    Added search among channels to SearchService Added channel search snippet

--- src/CoubSharp/Managers/SearchService.cs
+++ src/CoubSharp/Managers/SearchService.cs
@@ -21,6 +21,12 @@ namespace CoubSharp.Managers
 
         }
 
+        public static class ChannelSearchOrderBy
+        {
+            public const string FollowersCount = "followers_count";
+            public const string Newest = "newest";
+        }
+
         internal const string SearchUrlBase = "/api/v2/search";
 
         public SearchService()
@@ -33,6 +39,22 @@ namespace CoubSharp.Managers
             if (page < 0) throw new ArgumentOutOfRangeException("page", "page can't be negative");
             if (perPage < 0) throw new ArgumentOutOfRangeException("perPage", "perPage can't be negative");
             var url = $"{CoubService.ApiUrlBase}{SearchUrlBase}?q={search}&page={page}&per_page={perPage}&order_by={orderBy}";
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var json = await response.Content.ReadAsStringAsync();
+                var searchResult = JsonConvert.DeserializeObject<GeneralSearchResult>(json);
+                return searchResult;
+            }
+        }
+
+        public async Task<GeneralSearchResult> SearchChannelsAsync(string search, int page, int perPage, string orderBy)
+        {
+            if (string.IsNullOrWhiteSpace(search)) throw new ArgumentNullException("search", "search can't be null or empty");
+            if (page < 0) throw new ArgumentOutOfRangeException("page", "page can't be negative");
+            if (perPage < 0) throw new ArgumentOutOfRangeException("perPage", "perPage can't be negative");
+            var url = $"{CoubService.ApiUrlBase}{SearchUrlBase}/channels?q={search}&page={page}&per_page={perPage}&order_by={orderBy}";
             using (HttpClient httpClient = new HttpClient())
             {
                 HttpResponseMessage response = await httpClient.GetAsync(url);
